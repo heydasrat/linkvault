90adee8ca86a3f28f50df39d857c9ca31fe488c8
heydasrat <heydasrat@gmail.com>
2026-09-22 16:21:47 +0500

    feat: add cookie-parser and cors middleware for enhanced request handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,19 @@
 import express from 'express'
-
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
+// Configurations
+app.use(cors({
+    origin: config.cross_origin,
+    credentials: true
+}));
 
-export {app}
+app.use(express.json({ limit: "1mb" }))
+app.use(express.urlencoded({ extended: true, limit: "1mb" }))
+app.use(express.static("./public"));
+app.use(cookieParser())
+
+
+export { app }
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,6 +3,7 @@ dotenv.config()
 
 const config = {
     port: process.env.PORT,
-    mongodbUri: process.env.MONGODB_URI
+    mongodbUri: process.env.MONGODB_URI,
+    cross_origin:process.env.CROSS_ORIGIN
 }
 export default config;
